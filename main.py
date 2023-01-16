1d637afae61da1b9f4cbeaa371bb2998a4c7e283
YR <resumm.er09@gmail.com>
2023-01-16 19:17:33 +0900

    UI 임시

--- main.py
+++ main.py
@@ -1,13 +1,53 @@
-import pymysql
+import os
+import sys
+import time
 
-HOST = '10.10.21.110'
-PORT = 3306
-USER = 'user_t'
-PASSWORD = 'xlavmfhwprxm9'
+from PyQt5 import uic
+from PyQt5.QtGui import QFontDatabase, QFont
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem, QAbstractItemView, QMessageBox
+from matplotlib import font_manager
+from datetime import datetime, timedelta
 
-conn = pymysql.connect(host=HOST, port=PORT, user=USER, password=PASSWORD, db='smart', charset='utf8')
+# path = os.getcwd()
+# font_path = path + "\Pretendard-Light.otf"
+# font = font_manager.FontProperties(fname=font_path).get_name()
 
-## 연결 테스트문
-if conn.open:
-    with conn.cursor() as curs:
-        print('connected')
+form_class = uic.loadUiType("mainUI.ui")[0]
+
+# 메인 윈도우
+class WindowClass(QMainWindow, form_class):
+    user = None
+    today = datetime.now().date().strftime('%Y-%m-%d')
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    # 초기 화면 설정
+        self.Page.setCurrentWidget(self.p_intro)
+
+    # 로그인 / 로그아웃
+        self.btn_login.clicked.connect(self.login)
+        # self.btn_logout.clicked.connect(self.logout) 로그아웃 아직 없음
+        self.btn_join.clicked.connect(self.into_join)
+
+
+    def login(self):
+        print('로그인 함수')
+    def logout(self):
+        print('로그아웃 함수')
+
+    def into_join(self):
+        print('회원가입 페이지 이동')
+        self.Page.setCurrentWidget(self.p_join)
+
+if __name__ == "__main__":
+    # QApplication : 프로그램을 실행시켜주는 클래스
+    app = QApplication(sys.argv)
+    # WindowClass의 인스턴스 생성
+    mainWindow = WindowClass()
+
+    # 프로그램 화면을 보여주는 코드
+    mainWindow.show()
+    # 프로그램을 이벤트루프로 진입시키는(프로그램을 작동시키는) 코드
+    app.exec_()
